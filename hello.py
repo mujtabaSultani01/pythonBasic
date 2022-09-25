7224d89c1a5e5feeff6018b23c8d3a8c9652992e
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-09-25 10:56:10 +0330

    some basic math operations has been done...

--- hello.py
+++ hello.py
@@ -17,4 +17,17 @@ item2 = "gas"
 item3 = "groceries"
 print("the items are: ", item1, ",", item2, "and", item3)
 
+# finding the total distince between Kabul and Kandahar:
+kbl_ghazni = 200
+ghazni_zabul = 400
+zabul_kdr = 300
+kbl_kdr = kbl_ghazni + ghazni_zabul + zabul_kdr
+print("Total distince between Kabul to Kandahar is: ", kbl_kdr)
+
+#Now if you're going 70KM/hr, find the total time taken by your journey?
+speed_kmh = 70
+time_taken = kbl_kdr/speed_kmh
+print("Total time taken by your journey is: ", time_taken)
+#if you wanna to have two decimal points after dot(.) then you can use round function...
+print("Total time taken by your journey is: ", round(time_taken, 2))
 
