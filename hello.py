1811da017e07b924eb0988f58ec29580bfa4ea97
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-09-25 10:40:52 +0330

    python variable declaration (integer & string)

--- hello.py
+++ hello.py
@@ -1,3 +1,20 @@
 
 print("Helllloooo \n Pythonnnnnnnnn")
 print("if you add 3 and 5, the result would be: ", 3+5)
+
+# variable declaration...
+rent = 3000
+print("rent variable stored an integer number: ", rent)
+gas = 500
+groceries = 1200
+print("total expenses are: ", rent+gas+groceries)
+total = rent + gas + groceries
+print("total expenses is: ", total)
+
+#string values can be stored & displayed as follow
+item1 = "rent"
+item2 = "gas"
+item3 = "groceries"
+print("the items are: ", item1, ",", item2, "and", item3)
+
+
