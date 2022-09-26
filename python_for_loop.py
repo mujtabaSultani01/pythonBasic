242eeee339431de8a00dfe0970c6bf75d1b92702
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-09-26 15:40:42 +0330

    total monthly expense are added without using for loop...

--- python_for_loop.py
+++ python_for_loop.py
@@ -1,2 +1,6 @@
 # For working with for loop, let's have a simple problem:
 # Store monthly expenses in a list and find out the total expenses for all months.
+exp = [3200, 5400, 3400, 2800, 6300]
+total_exp = exp[0] + exp[1] + exp[2] + exp[3] + exp[4]
+print("Your total expenses for five months are: ", total_exp)
+
