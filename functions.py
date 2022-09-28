2b196aa697f3465882e7d1823a9a43b296677efd
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-09-28 14:46:30 +0330

    creating private modul concept has cleared...

---
+++ functions.py
@@ -0,0 +1,9 @@
+# Private modul concept...
+
+# return the area of triangle
+def calculate_triangle_area(base, height):
+    return 1/2 * (base*height)
+
+# return the area of square
+def calculate_square_area(length):
+    return length * length
---
+++ myProgram.py
@@ -0,0 +1,11 @@
+# Check the private modul concept.
+import functions
+
+triangle_area = functions.calculate_triangle_area(4,5)
+square_area = functions.calculate_square_area(6)
+
+print("Area of triangle (4,5) is: ", triangle_area)
+print("Area of square (6) is: ", square_area)
+
+# you can import moduls with names as you want, like
+# import functions as fu
