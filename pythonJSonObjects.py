de4318133a60585a3453616c859ca324f2344020
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-09-29 07:18:26 +0330

    JSon data stored in .txt file.

--- pythonJSonObjects.py
+++ pythonJSonObjects.py
@@ -26,5 +26,26 @@ for key in JSon:
  numbers, strings, dictionaries and etc... JSon is just a concept. JSon is jsut basically a format which is implemented
  by different languages.
 """
+book = {}
+book["Ali"] = {
+    'name': "Ali",
+    'address': "Kabul",
+    'phone': "0708478493"
+}
+book["Ahmad"] = {
+    'name': "Ahmad",
+    'address': "Kandahar",
+    'phone': "0783746733"
+}
+# we can use python default JSon modul and then we will call dumps() method.
+# dumps() method take dictionary object as input then it's dumping it as string, that's why we have string here.
+# then it convert book dictionary object into string, and then it converted into JSon format.
+import json
+S = json.dumps(book)
+print(S)
 
+# now we'll save this address book in a book.txt file. when you refresh the page the address will be inserted.
 
+with open("C:\\users/Habib/PycharmProjects/pythonBasic/book.txt", "w") as f:
+    f.write(S)
+    
