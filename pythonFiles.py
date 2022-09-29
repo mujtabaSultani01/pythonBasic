b21dd41f88a4df2fdb84947e0ae98843351acf6c
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-09-29 08:03:20 +0330

    ...

--- pythonFiles.py
+++ pythonFiles.py
@@ -6,3 +6,15 @@ F.write("\nI love PHP.")
 F.close()
 
 # Reading file. // we want to read file 'funny1.txt' and count each line words...
+# one short display:
+F_read = open("C:\\users/Habib/PycharmProjects/pythonBasic/funny1.txt", "r")
+print(F_read.read())
+F_read.close()
+
+# split() method will split the stirng using input character and return a list (array) of words.
+F_read1 = open("C:\\users/Habib/PycharmProjects/pythonBasic/funny1.txt", "r")
+for line in F_read1:
+    tokens = line.split(' ')
+    #print(str(tokens))
+    print(len(tokens))
+F_read1.close()
