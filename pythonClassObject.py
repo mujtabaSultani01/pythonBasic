f6fb1314ba7c8734cb6e93da12e2f85df0d15e81
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-10-01 11:23:42 +0330

    Class and Object theoritical concept is cleared...

---
+++ pythonClassObject.py
@@ -0,0 +1,20 @@
+"""
+First let's talk about class concept, when you're talking about class, it's not specific to a Python, it's applied to
+most programming languages as well.
+So when you talk human being, every human has a common set of properties such as name, gender, occupation etc. and
+also every human will be doing some common activities such as speaking, doing work, sleep etc. so the 1st set of
+properties that we covered are called properties in Object Oriented Programming world, and the activities that human
+performs are called methods in Object Oriented Programming.
+Thus, properties and methods form a central entity called class.so here a human is a class, which has two central
+component which are properties and methods.
+Human:
+    Properties                           Methods
+    Name                                 Speaks
+    Gender                               Do work
+    Occupation                           Sleeps
+So class is nothing but abstraction of some entities which contains common properties & methods.
+So what's Object? object is nothing, but a specific instance of a class.
+Now let's write a class in python:
+"""
+
+
