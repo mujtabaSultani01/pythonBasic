40806a8e03a1fc372ff3325943c8e0281f9af57b
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-10-02 12:34:07 +0330

    Multiple inheritance has been completed...

--- pythonMultipleInheritance.py
+++ pythonMultipleInheritance.py
@@ -4,4 +4,19 @@ have two classes and you just want to inherit the properties & methods of those 
 you want to have your own customization. that's why you should use multiple inheritance. you can inherit your class
 from many classses as you want. after comma you can type any number of classes and it will work fine.
 """
+class Fathe():
+    def gardening(self):
+        print("I enjoy gardening...")
 
+class Mother():
+    def cooking(self):
+        print("I love cooking...")
+
+class Child(Fathe, Mother):
+    def sports(self):
+        print("I love playing cricket and volleyball.")
+
+C = Child()
+C.gardening()
+C.cooking()
+C.sports()
