1d89c5277c28a13b4638a9396ff5d09a787377c7
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-10-02 12:33:08 +0330

    Multiple inheritance theoritical concepts is cleared...

---
+++ pythonMultipleInheritance.py
@@ -0,0 +1,7 @@
+"""
+Here we're inheriting a class from two different classes, the benefits of multiple inheritance is, sometimes you
+have two classes and you just want to inherit the properties & methods of those classes, just to reuse the code & then
+you want to have your own customization. that's why you should use multiple inheritance. you can inherit your class
+from many classses as you want. after comma you can type any number of classes and it will work fine.
+"""
+
