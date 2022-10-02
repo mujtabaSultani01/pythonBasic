c487dd57f29aac0f7656f9e55a9a050a9bd7a7b2
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-10-02 11:55:46 +0330

    Inheritance theoritical concept is cleared...

---
+++ pythonInheritence.py
@@ -0,0 +1,13 @@
+"""
+When you think about vehicle, you have so many vehicles available for use such as car, trucks, motorcycle etc.
+but they provide the same purpose which is transportation, when you go to specific type of vehicle such as car and
+motorcycle, they have their own purposes and own characteristics, for example a car has four wheels, it has a roof,
+where motorcycle has only two wheels and it has to no roof, car has also specific usages such as commute to work or
+vacation with family where motorcycle is primarily used for road trip and racing. so assume in this diagram both of
+the these vehicles can be taught of sub classes of base class called vehicle, and they  share the same properties of
+vehicle class, which is to provide transportation, but on top of that same share properties they have their own
+specific characteristics and purposes such as wheels, roof difference and specific usages.
+so here you can say that both car and motorcycle are sub classes of class vehicle, or we can say these two classes
+inherit from vehicle class. so this is the initial definition of inheritance here.
+the way ou inherit a class form other class is: write tht name of a class in braces.
+"""
